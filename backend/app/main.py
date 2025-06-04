f6867138c6a3b351daee3ba4a4692c6c902ed2e4
Scott Trinh <scott@scotttrinh.com>
2025-06-04 16:03:13 -0400

    Remove docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
+import os
+
 import sentry_sdk
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.routing import APIRoute
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from app.api.main import api_router
 from app.core.config import settings
@@ -20,14 +23,25 @@ app = FastAPI(
     generate_unique_id_function=custom_generate_unique_id,
 )
 
-# Set all CORS enabled origins
-if settings.all_cors_origins:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.all_cors_origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+# Serve the React frontend application directly from your FastAPI server
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+frontend_dist_path = os.path.join(current_dir, "..", "..", "frontend", "dist")
+static_files_path = os.path.normpath(frontend_dist_path)
+index_path = os.path.join(static_files_path, "index.html")
+
+app.mount(
+    "/",
+    StaticFiles(
+        directory=static_files_path,
+        html=True,
+        check_dir=True,
+    ),
+    name="frontend",
+)
+
+@app.get("/{full_path:path}", include_in_schema=False)
+async def frontend_router_redirect(_: str):
+    return FileResponse(index_path)
