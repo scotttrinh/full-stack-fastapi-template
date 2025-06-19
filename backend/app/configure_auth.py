7b514d862eb1d47b60ffd7d5ea9ad07fad0434fd
Scott Trinh <scott@scotttrinh.com>
2025-06-19 16:25:10 -0400

    Configure auth better

--- backend/app/configure_auth.py
+++ backend/app/configure_auth.py
@@ -1,5 +1,3 @@
-import uuid
-
 import gel
 import gel.fastapi
 
@@ -7,20 +5,30 @@ from app.core.config import settings
 
 
 def configure_branch_auth() -> str:
-    """Configure branch authentication and return the signing key."""
+    """Configure branch authentication and return the signing key.
+
+When you run this script, it will reset all auth-related configuration to default,
+and cause existing auth tokens to be invalidated (new random signing key).
+
+You can set the GEL_AUTH_SIGNING_KEY environment variable to a specific value to
+use that signing key instead of generating a new one.
+"""
+
     db = gel.create_client()
-    signing_key = str(uuid.uuid4())
 
     db.execute(
         f"""
 # Reset all auth configs to their defaults
 configure current branch reset cfg::cors_allow_origins;
+configure current branch reset cfg::EmailProvider;
+configure current branch reset cfg::current_email_provider_name;
 configure current branch reset ext::auth::EmailPasswordProviderConfig;
 configure current branch reset ext::auth::ProviderConfig;
 configure current branch reset ext::auth::AuthConfig;
 
+# Configure core auth settings
 configure current branch set ext::auth::AuthConfig::app_name := "{settings.PROJECT_NAME}";
-configure current branch set ext::auth::AuthConfig::auth_signing_key := "{signing_key}";
+configure current branch set ext::auth::AuthConfig::auth_signing_key := "{settings.GEL_AUTH_SIGNING_KEY}";
 configure current branch set ext::auth::AuthConfig::allowed_redirect_urls := {{
   "http://127.0.0.1:8000",
   "http://localhost:8000",
@@ -33,9 +41,20 @@ configure current branch set cfg::cors_allow_origins := {{
   "http://testserver",
 }};
 
-configure current database insert ext::auth::EmailPasswordProviderConfig {{
+# Configure email and password auth provider
+configure current branch insert ext::auth::EmailPasswordProviderConfig {{
   require_verification := false
 }};
+
+# Configure email provider to use Mailpit
+configure current branch insert cfg::EmailProvider {{
+  name := "Mailpit",
+  sender := "noreply@example.com",
+  host := "localhost",
+  port := 1025,
+  validate_certs := false,
+}};
+configure current branch set cfg::current_email_provider_name := "Mailpit";
         """
     )
 
