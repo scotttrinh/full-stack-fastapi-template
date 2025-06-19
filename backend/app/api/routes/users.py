0e59dbd107b6d60b99781e8fd829b4f5775156b4
Scott Trinh <scott@scotttrinh.com>
2025-06-18 21:30:22 -0400

    Do not expose a direct user create route
    This is really just signup, so use /auth/register

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -2,6 +2,7 @@ import uuid
 from typing import Any
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
 
 from app.api.deps import (
     CurrentUser,
@@ -12,7 +13,9 @@ from app.models.data import User
 from app.models.utils import Collection, LimitOffsetPaginationDep
 from app.services.user import UserServiceDep
 
-router = APIRouter(prefix="/users", tags=["users"], dependencies=[g.auth.maybe_auth_token])
+router = APIRouter(
+    prefix="/users", tags=["users"], dependencies=[g.auth.maybe_auth_token]
+)
 
 
 @router.get("/", dependencies=[Depends(get_current_active_superuser)])
@@ -26,24 +29,12 @@ async def read_users(
     return await user_service.list_all(limit_offset_pagination)
 
 
-@router.post(
-    "/",
-    dependencies=[Depends(get_current_active_superuser)],
-)
-async def create_user(*, user_service: UserServiceDep, user_in: User):
-    """
-    Create new user.
-    """
-
-    user = await user_service.create(user_in)
-    return user
+class UserUpdate(BaseModel):
+    email: str | None = None
+    full_name: str | None = None
 
 
-class UserUpdate(User.__variants__.Base):
-    pass
-
-
-@router.patch("/me", dependencies=[g.auth.maybe_auth_token])
+@router.patch("/me")
 async def update_user_me(
     *,
     user_service: UserServiceDep,
@@ -90,7 +81,7 @@ async def read_user_by_id(
     """
     Get a specific user by id.
     """
-    if user_id != current_user.id:
+    if user_id != current_user.id and not current_user.is_superuser:
         raise HTTPException(
             status_code=403,
             detail="The user doesn't have enough privileges",
